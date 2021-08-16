76545c4e5b6d01fca6e39aa1518e09b9a9d4e985
swyoon <sangwoong24yoon@gmail.com>
2021-08-16 15:15:03 +0000

    add bilinear upsampling; compatibility to opset 11

--- onnx2keras/upsampling_layers.py
+++ onnx2keras/upsampling_layers.py
@@ -23,16 +23,15 @@ def convert_upsample(node, params, layers, lambda_func, node_name, keras_name):
             raise AttributeError('Unsupported number of inputs')
         scale = np.uint8(params['scales'][-2:])
     else:
-        # for opset version - 9+
-        # Upsample since opset version 9 uses input[1] as 'scales' instead of attributes.
-        scale = np.uint8(layers[node.input[1]][-2:])
+        scale = np.uint8(layers[node.input[2]][-2:])
 
     if params['mode'].decode('utf-8') != 'nearest':
-        logger.error('Cannot convert non-nearest upsampling.')
-        raise AssertionError('Cannot convert non-nearest upsampling')
+        interpolation = 'bilinear'
+    else:
+        interpolation = 'nearest'
 
     upsampling = keras.layers.UpSampling2D(
-        size=scale, name=keras_name
+        size=scale, name=keras_name, interpolation=interpolation
     )
 
     layers[node_name] = upsampling(layers[node.input[0]])
